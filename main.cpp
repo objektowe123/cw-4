a53a8188eb6a3de7a2560b12153c080a4e011521
objektowe123 <32263872+objektowe123@users.noreply.github.com>
2017-10-30 13:36:08 +0100

    cw-4
    na zajeciach

--- main.cpp
+++ main.cpp
@@ -1,9 +1,22 @@
 #include <iostream>
-
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <vector>
 using namespace std;
 
-int main()
+int main(int argc, char*argv[])
 {
-    cout << "Hello world!" << endl;
+    if(argc !=2)
+    {
+        return -1;
+    }
+    cout<<argv[1]<<endl;
+    Dane dane = wczytaj(argv[1]);
+    zapisz("out.csv",dane);
+    cout<<endl<<"Srednia: "<<Srednia(dane);
+    cout<<endl<<"Minimum: "<<Minimum(dane);
+    cout<<endl<<"Maksimum: "<<Maksimum(dane);
+    cout<<endl<<"Calka: "<<Calka(dane);
     return 0;
 }
